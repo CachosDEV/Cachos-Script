d0b5b687adb8a27ecb6b7b732339de5a352412e5
CachosDEV <andrerr11beiro2010@gmail.com>
2026-02-24 16:19:00 -0300

    ui-base.lua

---
+++ ui-base.lua
@@ -0,0 +1,20 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "MEU SCRIPT",
+    SubTitle = "by Dede7zinho777",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Theme = "Dark"
+})
+
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "home" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+return {
+    Fluent = Fluent,
+    Window = Window,
+    Tabs = Tabs
+}
